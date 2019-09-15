079a491e9a8c1f1d97012cf09c5088aca7bddd78
arnie b <arnold.bwaila@gmail.com>
2019-09-15 16:13:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
+require 'pry'
 # Add your code here
+class Dog 
+  attr_accessor :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+ def self.all
+  return @@all
+ end
+ def self.clear_all
+   @@all.clear
+ end
+
+def self.print_all
+    @@all.each do |dog|
+     puts"#{dog.name}"
+   end
+ end
+ 
+ def self.save
+     @@all << self
+ end
+end
+puppy = Dog.new('puppy')
+binding.pry
+Dog.all
